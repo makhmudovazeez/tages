b877be444f0e1eed87ef3f0f3f4261867e08ed09
makhmudovazeez <azeezmakhmudov@gmail.com>
2022-12-23 02:04:21 +0500

    download test

--- test/integration_test.go
+++ test/integration_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/makhmudovazeez/tages/proto/tages"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,6 +16,8 @@ import (
 var f string = "2.jpg"
 
 const fileChunk = 8192
+
+var ID string
 
 func TestUploadFile(t *testing.T) {
 	conn, err := grpc.Dial("127.0.0.1:8181", grpc.WithInsecure())
@@ -64,14 +67,16 @@ func TestUploadFile(t *testing.T) {
 
 	for _, req := range requests {
 		if err := stream.Send(req); err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 	}
 
-	_, err = stream.CloseAndRecv()
+	resp, err := stream.CloseAndRecv()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
+
+	ID = resp.Id
 }
 
 func TestGetFiles(t *testing.T) {
@@ -87,6 +92,48 @@ func TestGetFiles(t *testing.T) {
 	defer cancel()
 
 	if _, err := client.GetFiles(ctx, &emptypb.Empty{}); err != nil {
-		t.Error(err)
+		t.Fatal(err)
+	}
+}
+
+func TestDownload(t *testing.T) {
+	conn, err := grpc.Dial("127.0.0.1:8181", grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := tages.NewTagesClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	stream, err := client.Download(ctx, &tages.DownloadRequest{
+		Id: ID,
+	})
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	_, err = stream.Recv()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for {
+		_, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("received all chunks")
+			break
+		}
+		if err != nil {
+			t.Fatal(err)
+			break
+		}
+	}
+
+	if err := stream.CloseSend(); err != nil {
+		t.Fatal(err)
 	}
 }
